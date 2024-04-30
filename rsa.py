af1a53a419073af475f60210628fa638a05443c2
269460 <134083681+269460@users.noreply.github.com>
2024-04-30 17:26:52 +0200

    rsa.py

--- rsa.py
+++ rsa.py
@@ -31,19 +31,19 @@ def rsa_encrypt(data, public_key):
         encrypted_data.extend(encrypted_val.to_bytes(byte_length, byteorder='big'))
     return bytes(encrypted_data)
 
-def rsa_decrypt(data, private_key):
-    d, n = private_key
-    byte_length = (n.bit_length() + 7) // 8
-    decrypted_data = bytearray()
-    for i in range(0, len(data), byte_length):
-        encrypted_val = int.from_bytes(data[i:i+byte_length], byteorder='big')
-        try:
-            decrypted_val = pow(encrypted_val, d, n)
-            decrypted_data.append(decrypted_val % 256)
-        except Exception as e:
-            print(f"Błąd przy deszyfrowaniu danych: {e}")
-            break
-    return bytes(decrypted_data)
+# def rsa_decrypt(data, private_key):
+#     d, n = private_key
+#     byte_length = (n.bit_length() + 7) // 8
+#     decrypted_data = bytearray()
+#     for i in range(0, len(data), byte_length):
+#         encrypted_val = int.from_bytes(data[i:i+byte_length], byteorder='big')
+#         try:
+#             decrypted_val = pow(encrypted_val, d, n)
+#             decrypted_data.append(decrypted_val % 256)
+#         except Exception as e:
+#             print(f"Błąd przy deszyfrowaniu danych: {e}")
+#             break
+#     return bytes(decrypted_data)
 
 
 
@@ -116,53 +116,53 @@ def modify_png(file_path, public_key, save_path):
     print("Plik został pomyślnie zmodyfikowany i zapisany.")
 
 
-def decrypt_and_reconstruct_png(encrypted_png_path, private_key, decrypted_png_path):
-    data = read_png(encrypted_png_path)
-    new_png_data = bytearray(data[:8])  # Kopiowanie nagłówka PNG
-    pos = 8
+# def decrypt_and_reconstruct_png(encrypted_png_path, private_key, decrypted_png_path):
+#     data = read_png(encrypted_png_path)
+#     new_png_data = bytearray(data[:8])  # Kopiowanie nagłówka PNG
+#     pos = 8
 
-    while pos < len(data):
-        chunk_len = struct.unpack('>I', data[pos:pos+4])[0]
-        pos += 4
-        chunk_type = data[pos:pos+4]
-        pos += 4
-        chunk_data = data[pos:pos+chunk_len]
-        pos += chunk_len
-        crc = data[pos:pos+4]
-        pos += 4
+#     while pos < len(data):
+#         chunk_len = struct.unpack('>I', data[pos:pos+4])[0]
+#         pos += 4
+#         chunk_type = data[pos:pos+4]
+#         pos += 4
+#         chunk_data = data[pos:pos+chunk_len]
+#         pos += chunk_len
+#         crc = data[pos:pos+4]
+#         pos += 4
 
-        if chunk_type == b'IDAT':
-            try:
-                decompressed_data = zlib.decompress(chunk_data)
-                decrypted_data = rsa_decrypt(decompressed_data, private_key)
-                recompressed_data = zlib.compress(decrypted_data)
-                chunk_data = recompressed_data
-                chunk_len = len(chunk_data)
-            except zlib.error as e:
-                print(f"Błąd dekompresji danych: {e}")
-                continue
-            except Exception as e:
-                print(f"Błąd przy deszyfrowaniu danych: {e}")
-                continue
+#         if chunk_type == b'IDAT':
+#             try:
+#                 decompressed_data = zlib.decompress(chunk_data)
+#                 decrypted_data = rsa_decrypt(decompressed_data, private_key)
+#                 recompressed_data = zlib.compress(decrypted_data)
+#                 chunk_data = recompressed_data
+#                 chunk_len = len(chunk_data)
+#             except zlib.error as e:
+#                 print(f"Błąd dekompresji danych: {e}")
+#                 continue
+#             except Exception as e:
+#                 print(f"Błąd przy deszyfrowaniu danych: {e}")
+#                 continue
 
-        new_crc = zlib.crc32(chunk_type)
-        new_crc = zlib.crc32(chunk_data, new_crc)
-        new_crc = struct.pack('>I', new_crc & 0xffffffff)
-        new_png_data.extend(struct.pack('>I', chunk_len))
-        new_png_data.extend(chunk_type)
-        new_png_data.extend(chunk_data)
-        new_png_data.extend(new_crc)
+#         new_crc = zlib.crc32(chunk_type)
+#         new_crc = zlib.crc32(chunk_data, new_crc)
+#         new_crc = struct.pack('>I', new_crc & 0xffffffff)
+#         new_png_data.extend(struct.pack('>I', chunk_len))
+#         new_png_data.extend(chunk_type)
+#         new_png_data.extend(chunk_data)
+#         new_png_data.extend(new_crc)
 
-    with open(decrypted_png_path, 'wb') as file:
-        file.write(new_png_data)
-    print("Plik został pomyślnie odszyfrowany i zapisany.")
+#     with open(decrypted_png_path, 'wb') as file:
+#         file.write(new_png_data)
+#     print("Plik został pomyślnie odszyfrowany i zapisany.")
 
 
 # Użycie:
 public_key, private_key = generate_rsa_keys(bits=512)
 original_png_path = "example2.png"
 encrypted_png_path = "encrypted_example2.png"
-decrypted_png_path = "decrypted_example2.png"
+#decrypted_png_path = "decrypted_example2.png"
 
 modify_png(original_png_path, public_key, encrypted_png_path)
-decrypt_and_reconstruct_png(encrypted_png_path, private_key, decrypted_png_path)
+#decrypt_and_reconstruct_png(encrypted_png_path, private_key, decrypted_png_path)
